fc2bd9c01433f3f1c5c05ef1f62542c481ed5096
Jake Porter <jakeporter310@gmail.com>
2021-11-14 21:04:29 -0500

    add completed class

--- StringSet/src/StringSet.java
+++ StringSet/src/StringSet.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class StringSet {
+import java.util.ArrayList;
+
+public class StringSet extends ArrayList<String> {
+
+    public boolean add(String newStr) {
+        try {
+            super.add(newStr);
+        } catch (RuntimeException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void pop() {
+        if (size() > 0) {
+            remove(size() - 1);
+        }
+    }
+
+    public int sumValidIntegers() {
+        int total = 0;
+        int num;
+        for (int i = 0; i < size(); i++) {
+            try {
+                num = Integer.parseInt(get(i));
+                total += num;
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+        return total;
+    }
+
+    public int numChars() {
+        int total = 0;
+        for (int i = 0; i < size(); i++) {
+            total += get(i).length();
+        }
+        return total;
+    }
+
+    public int countStrings(String stringToLookFor) {
+        int total = 0;
+        for (int i = 0; i < size(); i++) {
+            if (get(i).contains(stringToLookFor)) {
+                total++;
+            }
+        }
+        return total;
+    }
 }
