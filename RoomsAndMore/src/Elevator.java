94d6aa22e90a9bc5eb1ef2bc8f7e99cbeef87140
Caeosorio <94332937+Caeosorio@users.noreply.github.com>
2021-11-14 22:25:41 -0500

    Update Elevator.java

--- RoomsAndMore/src/Elevator.java
+++ RoomsAndMore/src/Elevator.java
@@ -1,2 +1,21 @@
-public class Elevator {
+public class Elevator extends Room {
+
+   private int currentFloor;
+
+   public Elevator(int area) {
+       super(area);
+   }
+  
+   public void up(int floors) {
+       this.currentFloor += floors;
+   }
+  
+   public void down(int floors) {
+       this.currentFloor -= floors;
+   }
+  
+   @Override
+   public String toString() {
+       return "Elevator:\n" + super.toString() + "\nCurrent Floor: " + this.currentFloor + "\n";
+   }
 }
