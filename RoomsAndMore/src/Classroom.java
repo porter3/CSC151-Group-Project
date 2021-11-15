9fc384a9b9fdafc99da23582d0351e74def621e4
Caeosorio <94332937+Caeosorio@users.noreply.github.com>
2021-11-14 22:15:52 -0500

    Update Classroom.java

--- RoomsAndMore/src/Classroom.java
+++ RoomsAndMore/src/Classroom.java
@@ -1,2 +1,32 @@
-public class Classroom {
+public class Classroom extends Room {
+
+   private int noOfChairs;
+  
+   public Classroom(int area) {
+       super(area);
+   }
+
+   public Classroom(int area, int noOfChairs) {
+       super(area);
+       this.noOfChairs = noOfChairs;
+   }
+
+   public int getNoOfChairs() {
+       return noOfChairs;
+   }
+
+
+   public void setNoOfChairs(int noOfChairs) {
+       this.noOfChairs = noOfChairs;
+   }
+
+   public int getCapacity() {
+       return this.noOfChairs;
+   }
+
+   @Override
+   public String toString() {
+       return "Classroom:\n" + super.toString() + "\n# of Chairs: " + this.noOfChairs + "\n";
+   }
 }
+
