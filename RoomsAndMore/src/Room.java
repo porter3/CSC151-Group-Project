9b2cf762c9db307213bf60625c1bbe24b1e67c27
Caeosorio <94332937+Caeosorio@users.noreply.github.com>
2021-11-14 22:11:42 -0500

    Update Room.java

--- RoomsAndMore/src/Room.java
+++ RoomsAndMore/src/Room.java
@@ -1,2 +1,19 @@
 public class Room {
+   private int area;
+   public Room(int area) {
+       this.area = area;
+   }
+ 
+   public int getSquareFeet() {
+       return this.area;
+   }
+  
+   public int getCapacity() {
+       return (this.area / 9);
+   }
+
+   @Override
+   public String toString() {
+       return "Area: " + this.area + " square feet\tCapacity: " + getCapacity();
+   }
 }
